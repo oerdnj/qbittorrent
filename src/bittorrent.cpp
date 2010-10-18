b8941bd280785c4f6a5b3ada01f2af49fb6fa85b
Cristian Greco <cristian@regolo.cc>
2010-10-18 02:54:11 +0200

    Imported Upstream version 2.4.6

--- src/bittorrent.cpp
+++ src/bittorrent.cpp
@@ -864,7 +864,7 @@ QTorrentHandle Bittorrent::addMagnetUri(QString magnet_uri, bool resumed) {
   }
   QString torrent_name = misc::magnetUriToName(magnet_uri);
   const QString savePath(getSavePath(hash, false, QString::null, torrent_name));
-  if(!defaultTempPath.isEmpty() && !TorrentPersistentData::isSeed(hash)) {
+  if(!defaultTempPath.isEmpty() && !TorrentPersistentData::isSeed(hash) && resumed) {
     qDebug("addMagnetURI: Temp folder is enabled.");
     qDebug("addTorrent::Temp folder is enabled.");
     QString torrent_tmp_path = defaultTempPath.replace("\\", "/");
@@ -913,10 +913,10 @@ QTorrentHandle Bittorrent::addMagnetUri(QString magnet_uri, bool resumed) {
   Q_ASSERT(h.hash() == hash);
 
   // If temp path is enabled, move torrent
-  /*if(!defaultTempPath.isEmpty() && !resumed) {
+  if(!defaultTempPath.isEmpty() && !resumed) {
     qDebug("Temp folder is enabled, moving new torrent to temp folder");
     h.move_storage(defaultTempPath);
-  }*/
+  }
 
   // Connections limit per torrent
   h.set_max_connections(Preferences::getMaxConnecsPerTorrent());
@@ -1116,7 +1116,7 @@ QTorrentHandle Bittorrent::addTorrent(QString path, bool fromScanDir, QString fr
   } else {
     savePath = getSavePath(hash, fromScanDir, path, root_folder);
   }
-  if(!defaultTempPath.isEmpty() && !TorrentPersistentData::isSeed(hash)) {
+  if(!defaultTempPath.isEmpty() && !TorrentPersistentData::isSeed(hash) && resumed) {
     qDebug("addTorrent::Temp folder is enabled.");
     QString torrent_tmp_path = defaultTempPath.replace("\\", "/");
     if(!root_folder.isEmpty()) {
@@ -1172,7 +1172,7 @@ QTorrentHandle Bittorrent::addTorrent(QString path, bool fromScanDir, QString fr
   TorrentPersistentData::setRootFolder(hash, root_folder);
 
   // If temp path is enabled, move torrent
-  /*if(!defaultTempPath.isEmpty() && !resumed) {
+  if(!defaultTempPath.isEmpty() && !resumed) {
     qDebug("Temp folder is enabled, moving new torrent to temp folder");
     QString torrent_tmp_path = defaultTempPath.replace("\\", "/");
     if(!root_folder.isEmpty()) {
@@ -1180,7 +1180,7 @@ QTorrentHandle Bittorrent::addTorrent(QString path, bool fromScanDir, QString fr
       torrent_tmp_path += root_folder;
     }
     h.move_storage(torrent_tmp_path);
-  }*/
+  }
 
   // Connections limit per torrent
   h.set_max_connections(Preferences::getMaxConnecsPerTorrent());
