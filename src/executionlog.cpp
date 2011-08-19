0b977a12fc6da184d26aa291efabd4de99da39ed
Cristian Greco <cristian@regolo.cc>
2011-08-19 23:50:39 +0200

    Imported Upstream version 2.8.4

--- src/executionlog.cpp
+++ src/executionlog.cpp
@@ -1,3 +1,35 @@
+/*
+ * Bittorrent Client using Qt4 and libtorrent.
+ * Copyright (C) 2011  Christophe Dumez
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * In addition, as a special exception, the copyright holders give permission to
+ * link this program with the OpenSSL project's "OpenSSL" library (or with
+ * modified versions of it that use the same license as the "OpenSSL" library),
+ * and distribute the linked executables. You must obey the GNU General Public
+ * License in all respects for all of the code used other than "OpenSSL".  If you
+ * modify file(s), you may extend this exception to your version of the file(s),
+ * but you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ *
+ * Contact : chris@qbittorrent.org
+ */
+
+#include <QListWidgetItem>
+#include <QLabel>
 #include "executionlog.h"
 #include "ui_executionlog.h"
 #include "qbtsession.h"
@@ -10,9 +42,13 @@ ExecutionLog::ExecutionLog(QWidget *parent) :
     ui->setupUi(this);
     ui->tabConsole->setTabIcon(0, IconProvider::instance()->getIcon("view-calendar-journal"));
     ui->tabConsole->setTabIcon(1, IconProvider::instance()->getIcon("view-filter"));
-    ui->textConsole->setHtml(QBtSession::instance()->getConsoleMessages().join("<br>"));
+    const QStringList log_msgs = QBtSession::instance()->getConsoleMessages();
+    foreach(const QString& msg, log_msgs)
+      addLogMessage(msg);
+    const QStringList ban_msgs = QBtSession::instance()->getPeerBanMessages();
+    foreach(const QString& msg, ban_msgs)
+      addBanMessage(msg);
     connect(QBtSession::instance(), SIGNAL(newConsoleMessage(QString)), SLOT(addLogMessage(QString)));
-    ui->textBannedPeers->setHtml(QBtSession::instance()->getPeerBanMessages().join("<br>"));
     connect(QBtSession::instance(), SIGNAL(newBanMessage(QString)), SLOT(addBanMessage(QString)));
 }
 
@@ -23,10 +59,24 @@ ExecutionLog::~ExecutionLog()
 
 void ExecutionLog::addLogMessage(const QString &msg)
 {
-   ui->textConsole->setHtml(msg+ui->textConsole->toHtml());
+   QListWidgetItem *item = new QListWidgetItem;
+   QLabel *lbl = new QLabel(msg);
+   lbl->setContentsMargins(4, 2, 4, 2);
+   item->setSizeHint(lbl->sizeHint());
+   ui->logList->insertItem(0, item);
+   ui->logList->setItemWidget(item, lbl);
+   if(ui->logList->count() > MAX_LOG_MESSAGES)
+     delete ui->logList->takeItem(ui->logList->count()-1);
 }
 
 void ExecutionLog::addBanMessage(const QString &msg)
 {
-   ui->textBannedPeers->setHtml(msg+ui->textBannedPeers->toHtml());
+  QListWidgetItem *item = new QListWidgetItem;
+  QLabel *lbl = new QLabel(msg);
+  lbl->setContentsMargins(4, 2, 4, 2);
+  item->setSizeHint(lbl->sizeHint());
+  ui->banList->insertItem(0, item);
+  ui->banList->setItemWidget(item, lbl);
+  if(ui->banList->count() > MAX_LOG_MESSAGES)
+    delete ui->banList->takeItem(ui->banList->count()-1);
 }
--- src/qtlibtorrent/qbtsession.cpp
+++ src/qtlibtorrent/qbtsession.cpp
@@ -392,7 +392,8 @@ void QBtSession::configureSession() {
   //sessionSettings.announce_to_all_trackers = true;
   sessionSettings.auto_scrape_interval = 1200; // 20 minutes
 #if LIBTORRENT_VERSION_MINOR > 14
-  sessionSettings.announce_to_all_trackers = true;
+  // Comply with the multi-tracker specification
+  sessionSettings.announce_to_all_trackers = false;
   sessionSettings.announce_to_all_tiers = false;
   sessionSettings.auto_scrape_min_interval = 900; // 15 minutes
 #endif
@@ -1692,25 +1693,25 @@ void QBtSession::addConsoleMessage(QString msg, QString) {
   emit newConsoleMessage(QDateTime::currentDateTime().toString("dd/MM/yyyy hh:mm:ss") + " - " + msg);
 #else
 void QBtSession::addConsoleMessage(QString msg, QColor color) {
-  if(consoleMessages.size() > 100) {
-    consoleMessages.removeLast();
+  if(consoleMessages.size() > MAX_LOG_MESSAGES) {
+    consoleMessages.removeFirst();
   }
   msg = "<font color='grey'>"+ QDateTime::currentDateTime().toString(QString::fromUtf8("dd/MM/yyyy hh:mm:ss")) + "</font> - <font color='" + color.name() + "'><i>" + msg + "</i></font>";
-  consoleMessages.prepend(msg);
+  consoleMessages.append(msg);
   emit newConsoleMessage(msg);
 #endif
 }
 
 void QBtSession::addPeerBanMessage(QString ip, bool from_ipfilter) {
-  if(peerBanMessages.size() > 100) {
-    peerBanMessages.removeLast();
+  if(peerBanMessages.size() > MAX_LOG_MESSAGES) {
+    peerBanMessages.removeFirst();
   }
   QString msg;
   if(from_ipfilter)
     msg = "<font color='grey'>" + QDateTime::currentDateTime().toString(QString::fromUtf8("dd/MM/yyyy hh:mm:ss")) + "</font> - " + tr("<font color='red'>%1</font> <i>was blocked due to your IP filter</i>", "x.y.z.w was blocked").arg(ip);
   else
     msg = "<font color='grey'>" + QDateTime::currentDateTime().toString(QString::fromUtf8("dd/MM/yyyy hh:mm:ss")) + "</font> - " + tr("<font color='red'>%1</font> <i>was banned due to corrupt pieces</i>", "x.y.z.w was banned").arg(ip);
-  peerBanMessages.prepend(msg);
+  peerBanMessages.append(msg);
   emit newBanMessage(msg);
 }
 
